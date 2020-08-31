5bad51a80c1a8e8a6ff82ed99ab656c99b1cd0c9
Ramya <70519790+ramyamuthusaravanan@users.noreply.github.com>
2020-08-31 22:09:55 +0530

    Add files via upload

---
+++ printformat.py
@@ -0,0 +1,12 @@
+#Write a Python Program to print the given string in the format specified in the ​sample output. 
+# WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a 
+#SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens 
+ 
+#Sample Output: 
+ 
+#WE, THE PEOPLE OF INDIA,   
+#	having solemnly resolved to constitute India into a SOVEREIGN, !  
+#		SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC    
+#		and to secure to all its citizens
+
+print("WE, THE PEOPLE OF INDIA,\n \thaving solemnly resolved to constitute India into a SOVEREIGN,!\n \t\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\n \t\tand to secure to all its citizens")
