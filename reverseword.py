ed457bc7e83ef559b81433fe9482adc94da44569
Ramya <70519790+ramyamuthusaravanan@users.noreply.github.com>
2020-08-31 21:59:31 +0530

    Add files via upload

---
+++ reverseword.py
@@ -0,0 +1,9 @@
+#Write a Python program to reverse a word after accepting the input from the user. 
+ 
+#Sample Output: 
+ 
+#Input word: AcadGild 
+#Output: dilGdacA 
+ 
+word=(input("Enter a word : "))
+print(word[::-1])
